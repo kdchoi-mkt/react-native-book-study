f84eac15f0b485960ba726c62d52b2542aeb7ca4
kdchoi-mkt <valuekds@naver.com>
2020-07-11 00:09:56 +0900

    inline container style

--- firstProject/App.js
+++ firstProject/App.js
@@ -39,6 +39,9 @@ const App: () => React$Node = () => {
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
+          <View style = {{backgroundColor: 'blue', flex : 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text style = {{color: '#fff', fontSize:22}}>Hello world! This is test for inline style</Text>
+          </View>
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
