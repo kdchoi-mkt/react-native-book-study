8e1bd18cf7e6c0ab5ee1da93f9ad25a4b2d3de75
kdchoi-mkt <valuekds@naver.com>
2020-07-16 00:25:04 +0900

    add: try to componentization

--- firstProject/App.js
+++ firstProject/App.js
@@ -26,125 +26,34 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const btnStyles = StyleSheet.create({
-  core: {
-    borderStyle: 'solid',
-    borderColor: '#d5d5d5',
-    borderWidth: 5,
-    backgroundColor: '#eee',
-    borderRadius: 3,
-    padding: 3,
-    paddingLeft: 5,
-    paddingRight: 5
-  },
-  primary: {
-    backgroundColor: '#60b044',
-    borderColor: '#5ca941'
-  },
-  hairlineBorder: {
-    borderWidth: StyleSheet.hairlineWidth
-  },
-  spacer: {
-    marginBottom: 10
-  }
-});
 
-
-const viewStyles = {
-  flex: 11,
-  justifyContent: 'center',
-  alignItems: 'center'
+const BasicType = () => {
+  <Text style = {styles.headline}>
+  Welcome to <Text style = {styles.bold}> REACT </Text> native {'\n'}
+  <Text style = {styles.subheader}>This is <Text style = {styles.bold}>so cool</Text>
+  </Text>
+</Text>
 }
-
-const textStyles = {
-  color: '#fff',
-  fontSize: 50
-};
-
-const btn = {
-  borderStyle: 'solid',
-  borderColor: '#d5d5d5',
-  borderWidth: 12,
-  backgroundColor: '#eee',
-  borderRadius: 40,
-  padding: 20,
-  paddingLeft: 100,
-  paddingRight: 100
-};
-
-const btnPrimary = {
-  backgroundColor: '#60b044',
-  borderColor: '#5ca941'
-}
-
-const new_styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    paddingVertical: 20,
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  content: {
-    padding: 20,
-    margin: 0,
-    marginHorizontal: 20,
-    backgroundColor: '#ef4c',
-    width: 125,
-    height: 125,
-    borderWidth: 1,
-    borderColor: 'red',
-    textAlign: 'center'
-  }
-})
 
 const App: () => React$Node = () => {
   console.log('Debugging from RN');
   return (
-    <View style = {new_styles.main} >
-      <Text style = {new_styles.content} >Hello1</Text>
-      <Text style = {new_styles.content} >Hello2</Text>
-      <Text style = {new_styles.content} >Hello3</Text>
-    </View>
+    BasicType()
    );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  headline: {
+    fontFamily: 'Georgia',
+    fontSize: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  subheader: {
+    color: 'blue'
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  bold: {
+    fontWeight: 'bold'
+  }
 });
+
 
 export default App;
