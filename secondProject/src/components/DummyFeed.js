6cd81e60a35e2324c13b1a190c874f7957d16e55
kdchoi-mkt <valuekds@naver.com>
2020-07-27 01:12:24 +0900

    fix: changed dummy datas

---
+++ secondProject/src/components/DummyFeed.js
@@ -0,0 +1,45 @@
+import DummyString from './LoremIpsum'
+import url from './SampleImage'
+
+export default DummyFeed = [
+    {
+        imageUrl: url,
+        title: 'Hello1',
+        description: DummyString,
+        date: Date(),
+        author: 'Hello',
+        location: 'Hello',
+        index: 1,
+        id: '1'
+    },
+    {
+        imageUrl: url,
+        title: 'Hello2',
+        description: DummyString,
+        date: Date(),
+        author: 'Hello',
+        location: 'Hello',
+        index: 2,
+        id: '2'
+    },
+    {
+        imageUrl: url,
+        title: 'Hello3',
+        description: DummyString,
+        date: Date(),
+        author: 'Hello',
+        location: 'Hello',
+        index: 3,
+        id: '3'
+    },
+    {
+        imageUrl: url,
+        title: 'Hello4',
+        description: DummyString,
+        date: Date(),
+        author: 'Hello',
+        location: 'Hello',
+        index: 4,
+        id: '4'
+    }
+]
