8af8190401a591f9cb6e99bcfa2e231c6830753e
kdchoi-mkt <valuekds@naver.com>
2020-07-27 01:49:50 +0900

    fix: 가독성있게 색깔 변경

--- secondProject/src/styles/global.js
+++ secondProject/src/styles/global.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native'
 
-export const BG_COLOR = '#343336'
+export const BG_COLOR = '#b3ffeb'
 export const BAR_COLOR = '#343336'
-export const TEXT_COLOR = '#343336'
-export const HEADER_TEXT_COLOR = '#343336'
+export const TEXT_COLOR = '#000000'
+export const HEADER_TEXT_COLOR = '#111111'
 export const MUTED_COLOR = '#343336'
 export const LINK_COLOR = '#343336'
 export const ACCENT_COLORS = ['#343336']
