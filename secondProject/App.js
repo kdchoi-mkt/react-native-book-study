1aba91b251f37339cfb5858fb63320dd7d4d4820
kdchoi-mkt <valuekds@naver.com>
2020-07-26 21:16:41 +0900

    test: blurRadius magnitudes

--- secondProject/App.js
+++ secondProject/App.js
@@ -16,13 +16,22 @@ import {
   StatusBar,
 } from 'react-native';
 
-import SmallText from './src/components/SmallText'
 import DummyString from './src/components/LoremIpsum'
+import url from './src/components/SampleImage'
+import Thumbnail from './src/components/Thumbnail'
 
 const App = () => {
   return (
     <View style={styles.mainView}>
-      <SmallText> {DummyString} </SmallText>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={0}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={1}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={2}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={3}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={4}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={5}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={6}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={7}/>
+      <Thumbnail url={url} titleText={DummyString} accentColor="#ff0000" blurRadius={8}/>
     </View>
   );
 };
@@ -31,8 +40,7 @@ const styles = StyleSheet.create({
   mainView: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row'
+    justifyContent: 'center'
   }
 });
 
