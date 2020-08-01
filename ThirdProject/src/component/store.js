42030bca9325b8c779447a2adf64718d0fe1c0b6
kdchoi-mkt <valuekds@naver.com>
2020-08-02 01:28:20 +0900

    refactor: flux => redux

--- ThirdProject/src/component/store.js
+++ ThirdProject/src/component/store.js
@@ -1,36 +1,5 @@
-import Dispatcher from './Dispatcher';
-import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
+import {createStore} from 'redux';
 
-let tally = {
-    count: 10
-};
-
-const increment = () => {
-    tally.count += 1;
-}
-
-const decrement = () => {
-    tally.count -= 1;
-}
-
-const zero = () => {
-    tally.count = 0;
-}
-
-class TallyStore extends EventEmitter{
-    getTally(){
-        return Object.assign({}, tally);
-    }
-    addChangeListener(callback) {
-        this.addListener('CHANGE', callback);
-    }
-    removeChangeListener(callback){
-        this.removeListener('CHANGE', callback);
-    }
-    emitChange() {
-        this.emit('CHANGE')
-    }
-}
 
 const initialState = {
     count: 0
@@ -55,7 +24,4 @@ const countReducer = (state = initialState, action) => {
     }
 };
 
-Dispatcher.register(handleAction);
-
-const instance = new TallyStore();
-export default instance;
+export default createStore(countReducer);
