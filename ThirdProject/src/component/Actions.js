c7f5a850ca9c7945b5550b97e346cad18b6c93c1
kdchoi-mkt <valuekds@naver.com>
2020-08-02 01:47:30 +0900

    detach dispatcher

--- ThirdProject/src/component/Actions.js
+++ ThirdProject/src/component/Actions.js
@@ -4,19 +4,19 @@ export const increment = () => {
     const action = {
         type: 'INCREMENT'
     };
-    Dispatcher.dispatch(action);
+    return action
 };
 
 export const decrement = () => {
     const action = {
         type: 'DECREMENT'
     };
-    Dispatcher.dispatch(action);
+    return action
 };
 
 export const zero = () => {
     const action = {
         type: 'ZERO'
     };
-    Dispatcher.dispatch(action);
+    return action
 };
